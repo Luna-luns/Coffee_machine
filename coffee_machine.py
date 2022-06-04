35e12695d459c8f88f68dfc867abcd500a9d79ec
Elizaveta Strunnikova <liza.strunnikova@yandex.ru>
2022-06-04 19:26:23 +0300

    Stage 2.Improved

--- coffee_machine.py
+++ coffee_machine.py
@@ -1,16 +1,28 @@
+WATER = 200
+MILK = 50
+BEANS = 15
+
+
 def ask_for_amount_of_coffee() -> str:
     return input('Write how many cups of coffee you will need:').strip()
 
 
-def count_needed_ingredients(_amount_of_coffee: int, water=200, milk=50, beans=15) -> str:
-    water *= _amount_of_coffee
-    milk *= _amount_of_coffee
-    beans *= _amount_of_coffee
-    if _amount_of_coffee == 1:
-        return f'For {_amount_of_coffee} cup of coffee you will need:\n{water} ml of water\n{milk} ml of milk\n{beans} g of coffee beans'
+def count_needed_ingredients(_amount_of_coffee: int) -> list:
+    water = WATER * _amount_of_coffee
+    milk = MILK * _amount_of_coffee
+    beans = BEANS * _amount_of_coffee
+    _ingredients = [water, milk, beans]
 
-    return f'For {_amount_of_coffee} cups of coffee you will need:\n{water} ml of water\n{milk} ml of milk\n{beans} g of coffee beans'
+    return _ingredients
+
+
+def show_needed_ingredients(_amount_of_coffee: int, _ingredients: list) -> None:
+    if _amount_of_coffee == 1:
+        print(f'For {_amount_of_coffee} cup of coffee you will need:\n{_ingredients[0]} ml of water\n{_ingredients[1]} ml of milk\n{_ingredients[2]} g of coffee beans')
+
+    print(f'For {_amount_of_coffee} cups of coffee you will need:\n{_ingredients[0]} ml of water\n{_ingredients[1]} ml of milk\n{_ingredients[2]} g of coffee beans')
 
 
 amount_of_coffee = int(ask_for_amount_of_coffee())
-print(count_needed_ingredients(amount_of_coffee))
+ingredients = count_needed_ingredients(amount_of_coffee)
+show_needed_ingredients(amount_of_coffee, ingredients)
